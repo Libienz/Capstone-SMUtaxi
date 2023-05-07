a063d6cf438bfd233df63ac3b1e6011f298c26be
Kuen Hee Lee <Leekh10195@gmail.com>
2023-05-07 18:14:50 +0900

    proj setting

---
+++ src/main/java/com/capstone/smutaxi/SmutaxiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.smutaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmutaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmutaxiApplication.class, args);
+	}
+
+}
