f6bb17e94f9b6bb443a82fa03ad53042e075a50b
ks0927 <brian_als@naver.com>
2023-05-22 22:44:17 +0900

    Test: add makeChatRoom()

---
+++ src/test/java/com/capstone/smutaxi/chat/ChattingTest.java
@@ -0,0 +1,44 @@
+package com.capstone.smutaxi.chat;
+
+import com.capstone.smutaxi.chat.domain.ChatRoom;
+import com.capstone.smutaxi.chat.repository.ChatRoomRepository;
+import com.capstone.smutaxi.chat.service.ChatRoomService;
+import com.capstone.smutaxi.chat.service.MessageService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ChattingTest {
+
+    @Autowired
+    ChatRoomService chatRoomService;
+
+    @Autowired
+    ChatRoomRepository chatRoomRepository;
+
+    @Autowired
+    MessageService messageService;
+
+
+    /**
+     * chotRoom을 만들고 해당 id가 같은지 확인
+     */
+    @Test
+    public void makeChatRoom(){
+        ChatRoom chatRoom1 = chatRoomService.createChatRoom("chatRoom1");
+        Long chatRoom1Id = chatRoom1.getId();
+
+        Optional<ChatRoom> byId = chatRoomRepository.findById(chatRoom1Id);
+
+        Assertions.assertThat(byId.get().getId()).isEqualTo(chatRoom1.getId());
+
+    }
+
+}
